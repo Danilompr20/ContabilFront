25621bdbbf4b93eb064374e081ca66106740cf8c
Danilo <Danilompr@hotmail.com>
2021-03-11 20:47:59 -0300

    initial

---
+++ src/app/EmpresaEstabelecimento/EmpresaEstabelecimento.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-EmpresaEstabelecimento',
+  templateUrl: './EmpresaEstabelecimento.component.html',
+  styleUrls: ['./EmpresaEstabelecimento.component.css']
+})
+export class EmpresaEstabelecimentoComponent implements OnInit {
+
+  public empresas:any ={
+
+  }
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
